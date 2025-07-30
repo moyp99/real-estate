44eff95211c78e254a6d7931a8623e6aa9d9ff76
moyp99 <moypg99@gmail.com>
2025-07-29 18:52:41 -0600

    fix: fixed type

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Loader } from '@googlemaps/js-api-loader'
 import { Property } from '../types/Property'
-import '../types/global'
 
 interface GoogleMapProps {
   properties: Property[]
